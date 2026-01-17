66007c6fbdd7f73556bd956f8c36dd5ce3e6ce2a
chawanvikash <chawanvikash30@gmail.com>
2026-01-18 00:45:04 +0530

    updated-cores1

--- Backend/server.js
+++ Backend/server.js
@@ -23,8 +23,23 @@ const adminRoute=require("./routes/adminRoute");
 const dashPostsRoute = require("./routes/dashPostsRoute"); 
 
 const sendOTP = require('./utils/email.js'); 
+const allowedOrigins = [
+  "http://localhost:5173",                          
+  "https://achievement-portal.vercel.app"          
+];
 
-app.use(cors({origin: ["http://localhost:5173","https://achievement-portal.vercel.app"],credentials: true}));
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
