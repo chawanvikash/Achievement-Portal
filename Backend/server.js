1503a55fa6aaf9e8547015e79b5b2c4e83e19faf
chawanvikash <chawanvikash30@gmail.com>
2026-01-18 00:33:12 +0530

    updated-cores

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ const dashPostsRoute = require("./routes/dashPostsRoute");
 
 const sendOTP = require('./utils/email.js'); 
 
-app.use(cors({origin: "http://localhost:5173",credentials: true}));
+app.use(cors({origin: ["http://localhost:5173","https://achievement-portal.vercel.app"],credentials: true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
