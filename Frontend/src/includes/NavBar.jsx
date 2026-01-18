e1061bb6fa17e217aae553495d5ff0e5b28f99f4
chawanvikash <chawanvikash30@gmail.com>
2026-01-18 10:48:16 +0530

    updated-navbar3

--- Frontend/src/includes/NavBar.jsx
+++ Frontend/src/includes/NavBar.jsx
@@ -11,43 +11,41 @@ import { FaUserCircle } from 'react-icons/fa';
 
 export default function NavBar() {
   return (
-    <div>
-      <Navbar expand="lg" className="bg-body-tertiary shadow-sm" fixed='top' style={{ backgroundColor: '#ffffff', minHeight: '70px' }}>
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            <img 
-              src={iiestlogo} 
-              alt="iiest" 
-              className='iiest-logo' 
-              style={{ width: '40px', height: '40px', objectFit: 'contain' }} 
-            />
-          </Navbar.Brand>
-          
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" style={{ backgroundColor: '#ffffff' }}>
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              
-              <NavDropdown title="Achievements" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/achievements/students">Students</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/achievements/faculty">Faculties</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/achievements/alumni">Alumni</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/achievements/Institute">Institute</NavDropdown.Item>
-              </NavDropdown>
-              
-              <Nav.Link as={Link} to="/contacts">Contacts</Nav.Link>
-            </Nav>
+    <Navbar expand="lg" className="bg-body-tertiary shadow-sm" fixed='top' style={{ backgroundColor: '#ffffff' }}>
+      <Container> {/* Container keeps content centered and aligned */}
+        <Navbar.Brand as={Link} to="/">
+          <img 
+            src={iiestlogo} 
+            alt="iiest" 
+            style={{ width: '40px', height: '40px', objectFit: 'contain' }} 
+          />
+        </Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* Use mx-auto here to center the main links */}
+          <Nav className="mx-auto"> 
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
             
-            <Nav>
-              <Nav.Link as={Link} to="/dashboard" className="d-flex align-items-center">
-                <FaUserCircle size={24} color="tomato" className="me-2"/> 
-                Profile
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+            <NavDropdown title="Achievements" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/achievements/students">Students</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/achievements/faculty">Faculties</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/achievements/alumni">Alumni</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/achievements/Institute">Institute</NavDropdown.Item>
+            </NavDropdown>
+            
+            <Nav.Link as={Link} to="/contacts">Contacts</Nav.Link>
+          </Nav>
+          
+          <Nav>
+            <Nav.Link as={Link} to="/dashboard" className="d-flex align-items-center">
+              <FaUserCircle size={24} color="tomato" className="me-2"/> 
+              Profile
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
